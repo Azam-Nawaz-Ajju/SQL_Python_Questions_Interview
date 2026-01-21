da68e1ceee07b56040d65a7096bf95c5776159d0
Azam Nawaz <azamnawaz2204@gmail.com>
2026-01-21 13:01:02 +0530

    adding leetcodeSQL

---
+++ LeetCodeEasy.sql
@@ -0,0 +1,605 @@
+    -- Question 21
+-- Table: ActorDirector
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | actor_id    | int     |
+-- | director_id | int     |
+-- | timestamp   | int     |
+-- +-------------+---------+
+-- timestamp is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor have cooperated with the director at least 3 times.
+
+-- Example:
+
+-- ActorDirector table:
+-- +-------------+-------------+-------------+
+-- | actor_id    | director_id | timestamp   |
+-- +-------------+-------------+-------------+
+-- | 1           | 1           | 0           |
+-- | 1           | 1           | 1           |
+-- | 1           | 1           | 2           |
+-- | 1           | 2           | 3           |
+-- | 1           | 2           | 4           |
+-- | 2           | 1           | 5           |
+-- | 2           | 1           | 6           |
+-- +-------------+-------------+-------------+
+
+-- Result table:
+-- +-------------+-------------+
+-- | actor_id    | director_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- +-------------+-------------+
+-- The only pair is (1, 1) where they cooperated exactly 3 times.
+
+
+SELECT 
+    actor_id,director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) = 3
+
+-- =============================================================================
+
+-- Question 13
+-- Table: Ads
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | ad_id         | int     |
+-- | user_id       | int     |
+-- | action        | enum    |
+-- +---------------+---------+
+-- (ad_id, user_id) is the primary key for this table.
+-- Each row of this table contains the ID of an Ad, the ID of a user and the action taken by this user regarding this Ad.
+-- The action column is an ENUM type of ('Clicked', 'Viewed', 'Ignored').
+ 
+
+-- A company is running Ads and wants to calculate the performance of each Ad.
+
+-- Performance of the Ad is measured using Click-Through Rate (CTR) where:
+
+
+
+-- Write an SQL query to find the ctr of each Ad.
+
+-- Round ctr to 2 decimal points. Order the result table by ctr in descending order and by ad_id in ascending order in case of a tie.
+
+-- The query result format is in the following example:
+
+-- Ads table:
+-- +-------+---------+---------+
+-- | ad_id | user_id | action  |
+-- +-------+---------+---------+
+-- | 1     | 1       | Clicked |
+-- | 2     | 2       | Clicked |
+-- | 3     | 3       | Viewed  |
+-- | 5     | 5       | Ignored |
+-- | 1     | 7       | Ignored |
+-- | 2     | 7       | Viewed  |
+-- | 3     | 5       | Clicked |
+-- | 1     | 4       | Viewed  |
+-- | 2     | 11      | Viewed  |
+-- | 1     | 2       | Clicked |
+-- +-------+---------+---------+
+-- Result table:
+-- +-------+-------+
+-- | ad_id | ctr   |
+-- +-------+-------+
+-- | 1     | 66.67 |
+-- | 3     | 50.00 |
+-- | 2     | 33.33 |
+-- | 5     | 0.00  |
+-- +-------+-------+
+-- for ad_id = 1, ctr = (2/(2+1)) * 100 = 66.67
+-- for ad_id = 2, ctr = (1/(1+2)) * 100 = 33.33
+-- for ad_id = 3, ctr = (1/(1+1)) * 100 = 50.00
+-- for ad_id = 5, ctr = 0.00, Note that ad_id = 5 has no clicks or views.
+-- Note that we don't care about Ignored Ads.
+-- Result table is ordered by the ctr. in case of a tie we order them by ad_id
+
+WITH ads_table as 
+(
+    SELECT 
+        ad_id, 
+        SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0) as ads_clicked,
+        SUM(CASE WHEN action in ('Clicked','Viewed') THEN 1 ELSE 0) as total_views
+    FROM ads
+    GROUP BY ad_id
+)
+SELECT ad_id, ROUND(
+                    (CASE WHEN total_views = 0 THEN 0.0
+                    ELSE (ads_clicked/total_views)* 100 END)
+                ,2) as Ctr 
+FROM ads_table
+ORDER BY ctr DESC, ad_id ASC; 
+
+
+
+-- ==========================================================================================
+-- Question 42
+-- Table: Views
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | article_id    | int     |
+-- | author_id     | int     |
+-- | viewer_id     | int     |
+-- | view_date     | date    |
+-- +---------------+---------+
+-- There is no primary key for this table, it may have duplicate rows.
+-- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+-- Note that equal author_id and viewer_id indicate the same person.
+ 
+
+-- Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.
+
+-- The query result format is in the following example:
+
+-- Views table:
+-- +------------+-----------+-----------+------------+
+-- | article_id | author_id | viewer_id | view_date  |
+-- +------------+-----------+-----------+------------+
+-- | 1          | 3         | 5         | 2019-08-01 |
+-- | 1          | 3         | 6         | 2019-08-02 |
+-- | 2          | 7         | 7         | 2019-08-01 |
+-- | 2          | 7         | 6         | 2019-08-02 |
+-- | 4          | 7         | 1         | 2019-07-22 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- +------------+-----------+-----------+------------+
+
+-- Result table:
+-- +------+
+-- | id   |
+-- +------+
+-- | 4    |
+-- | 7    |
+-- +------+
+
+
+SELECT
+    DISTINCT author_id as id 
+FROM views
+WHERE authord = viewer_id 
+ORDER BY author_id ASC
+
+
+-- ==================================================================================
+-- Question 39
+-- Table: Prices
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+-- Table: UnitsSold
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- There is no primary key for this table, it may contain duplicates.
+-- Each row of this table indicates the date, units and product_id of each product sold. 
+ 
+
+-- Write an SQL query to find the average selling price for each product.
+
+-- average_price should be rounded to 2 decimal places.
+
+-- The query result format is in the following example:
+
+-- Prices table:
+-- +------------+------------+------------+--------+
+-- | product_id | start_date | end_date   | price  |
+-- +------------+------------+------------+--------+
+-- | 1          | 2019-02-17 | 2019-02-28 | 5      |
+-- | 1          | 2019-03-01 | 2019-03-22 | 20     |
+-- | 2          | 2019-02-01 | 2019-02-20 | 15     |
+-- | 2          | 2019-02-21 | 2019-03-31 | 30     |
+-- +------------+------------+------------+--------+
+ 
+-- UnitsSold table:
+-- +------------+---------------+-------+
+-- | product_id | purchase_date | units |
+-- +------------+---------------+-------+
+-- | 1          | 2019-02-25    | 100   |
+-- | 1          | 2019-03-01    | 15    |
+-- | 2          | 2019-02-10    | 200   |
+-- | 2          | 2019-03-22    | 30    |
+-- +------------+---------------+-------+
+
+-- Result table:
+-- +------------+---------------+
+-- | product_id | average_price |
+-- +------------+---------------+
+-- | 1          | 6.96          |
+-- | 2          | 16.96         |
+-- +------------+---------------+
+-- Average selling price = Total Price of Product / Number of products sold.
+-- Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
+-- Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
+
+SELECT 
+    u.product_id, 
+    ROUND(
+            (
+            SUM(u.units * p.price) / sum(u.units)
+            )
+        ,2) as avg_price
+FROM UnitsSold u
+LEFT JOIN  prices p  
+    ON u.product_id = p.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY u.product_id
+ORDER BY u.product_id
+
+
+-- ============================================================================================
+-- Question 5
+-- There is a table World
+
+-- +-----------------+------------+------------+--------------+---------------+
+-- | name            | continent  | area       | population   | gdp           |
+-- +-----------------+------------+------------+--------------+---------------+
+-- | Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+-- | Albania         | Europe     | 28748      | 2831741      | 12960000      |
+-- | Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+-- | Andorra         | Europe     | 468        | 78115        | 3712000       |
+-- | Angola          | Africa     | 1246700    | 20609294     | 100990000     |
+-- +-----------------+------------+------------+--------------+---------------+
+-- A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+
+-- Write a SQL solution to output big countries' name, population and area.
+
+-- For example, according to the above table, we should output:
+
+-- +--------------+-------------+--------------+
+-- | name         | population  | area         |
+-- +--------------+-------------+--------------+
+-- | Afghanistan  | 25500100    | 652230       |
+-- | Algeria      | 37100000    | 2381741      |
+-- +--------------+-------------+--------------+
+
+SELECT 
+    name,population, area
+FORM world
+WHERE (area > 3000000 or population > 25000000)
+
+-- =================================================================================================
+
+-- Question 24
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
+
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
+SLECT max(num) as num
+FROM 
+(
+    SELECT 
+        num
+    FROM my_numbers 
+    GROUP BY num 
+    HAVING count(*) = 1
+)a 
+
+-- =====================================================================================
+
+-- Question7
+-- There is a table courses with columns: student and class
+
+-- Please list out all classes which have more than or equal to 5 students.
+
+-- For example, the table:
+
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+
+-- Solution
+SELECT 
+    class
+FROM courses
+GROUP BY class 
+HAVING COUNT(DISTINCT student) >= 5
+
+-- ====================================================================================
+-- Question 14
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the following information for each person in the Person table,
+-- regardless if there is an address for each of those people:
+
+-- FirstName, LastName, City, State
+
+SELECT 
+    p.FirstName, p.LastName, a.City, a.State
+FROM person p
+LEFT JOIN Address a 
+    ON p.person_id = a.person_id 
+
+
+-- =============================================================================================
+
+-- Question 37
+-- Several friends at a cinema ticket office would like to reserve consecutive available seats.
+-- Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
+-- | seat_id | free |
+-- |---------|------|
+-- | 1       | 1    |
+-- | 2       | 0    |
+-- | 3       | 1    |
+-- | 4       | 1    |
+-- | 5       | 1    |
+ 
+
+-- Your query should return the following result for the sample case above.
+ 
+
+-- | seat_id |
+-- |---------|
+-- | 3       |
+-- | 4       |
+-- | 5       |
+-- Note:
+-- The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
+-- Consecutive available seats are more than 2(inclusive) seats consecutively available.
+
+-- Solution
+
+WITH free_seats AS (
+    SELECT seat_id 
+    FROM cinema 
+    WHERE free = 1
+),
+rank_free_seats AS (
+    SELECT 
+        seat_id,
+        seat_id - ROW_NUMBER() OVER (ORDER BY seat_id) AS grp
+    FROM free_seats
+),
+grp_seat_count AS (
+    SELECT 
+        seat_id,
+        grp,
+        COUNT(seat_id) OVER(PARTITION BY grp) AS counts
+    FROM rank_free_seats
+)
+SELECT seat_id 
+FROM grp_seat_count 
+WHERE counts >= 3;
+
+-- ============================================================================================
+
+-- Question 2
+-- Table: Sessions
+
+-- +---------------------+---------+
+-- | Column Name         | Type    |
+-- +---------------------+---------+
+-- | session_id          | int     |
+-- | duration            | int     |
+-- +---------------------+---------+
+-- session_id is the primary key for this table.
+-- duration is the time in seconds that a user has visited the application.
+ 
+
+-- You want to know how long a user visits your application. You decided to create bins of "[0-5>", "[5-10>", "[10-15>" and "15 minutes or more" and count the number of sessions on it.
+
+-- Write an SQL query to report the (bin, total) in any order.
+
+-- The query result format is in the following example.
+
+-- Sessions table:
+-- +-------------+---------------+
+-- | session_id  | duration      |
+-- +-------------+---------------+
+-- | 1           | 30            |
+-- | 2           | 199           |
+-- | 3           | 299           |
+-- | 4           | 580           |
+-- | 5           | 1000          |
+-- +-------------+---------------+
+
+-- Result table:
+-- +--------------+--------------+
+-- | bin          | total        |
+-- +--------------+--------------+
+-- | [0-5>        | 3            |
+-- | [5-10>       | 1            |
+-- | [10-15>      | 0            |
+-- | 15 or more   | 1            |
+-- +--------------+--------------+
+
+-- For session_id 1, 2 and 3 have a duration greater or equal than 0 minutes and less than 5 minutes.
+-- For session_id 4 has a duration greater or equal than 5 minutes and less than 10 minutes.
+-- There are no session with a duration greater or equial than 10 minutes and less than 15 minutes.
+-- For session_id 5 has a duration greater or equal than 15 minutes.
+
+SELECT bin, COUNT(session_id) as total
+FROM
+(
+    SELECT 
+        session_id,
+        duration,
+        duration  / (60.0) as minutes,
+        CASE WHEN (duration / (60.0)) >= 0 AND (duration / (60.0)) < 5 THEN "[0-5>" 
+             WHEN (duration / (60.0)) >=5 AND (duration / (60.0)) < 10 THEN "[5-10>"
+             WHEN (duration / (60.0)) >=10 AND (duration / (60.0)) < 15 THEN "[10-15>"
+             ELSE "15 minutes or more"
+        END as bin
+    FROM sessions 
+) 
+GROUP BY bin
+
+
+-- =================================================================================================
+
+-- Question 8
+-- Query the customer_number from the orders table for the customer who has placed the largest number of orders.
+
+-- It is guaranteed that exactly one customer will have placed more orders than any other customer.
+
+-- The orders table is defined as follows:
+
+-- | Column            | Type      |
+-- |-------------------|-----------|
+-- | order_number (PK) | int       |
+-- | customer_number   | int       |
+-- | order_date        | date      |
+-- | required_date     | date      |
+-- | shipped_date      | date      |
+-- | status            | char(15)  |
+-- | comment           | char(200) |
+-- Sample Input
+
+-- | order_number | customer_number | order_date | required_date | shipped_date | status | comment |
+-- |--------------|-----------------|------------|---------------|--------------|--------|---------|
+-- | 1            | 1               | 2017-04-09 | 2017-04-13    | 2017-04-12   | Closed |         |
+-- | 2            | 2               | 2017-04-15 | 2017-04-20    | 2017-04-18   | Closed |         |
+-- | 3            | 3               | 2017-04-16 | 2017-04-25    | 2017-04-20   | Closed |         |
+-- | 4            | 3               | 2017-04-18 | 2017-04-28    | 2017-04-25   | Closed |         |
+-- Sample Output
+
+-- | customer_number |
+-- |-----------------|
+-- | 3               |
+-- Explanation
+
+-- The customer with number '3' has two orders, 
+-- which is greater than either customer '1' or '2' because each of them  only has one order. 
+-- So the result is customer_number '3'.
+
+
+WITH cust_order_cnt as 
+(
+SELECT 
+    customer_number, 
+    COUNT(order_number) as total_orders
+FROM orders
+GROUP BY customer_number 
+), 
+
+rank_custordercnt as 
+(
+SELECT customer_number, 
+    total_orders, 
+    DENSE_RANK() OVER (ORDER BY total_orders DESC) as rank  -- using dense rank in case two customers placed equal number of orders 
+FROM cust_order_cnt
+) 
+SELECT customer_number
+FROM rank_custordercnt
+WHERE rank = 1
+
+
+-- other approach 
+SELECT customer_number
+FROM cust_order_cnt 
+WHERE total_orders = SELECT MAX(total_orders) FROM cust_order_cnt 
+
+
+-- ===================================================================================================================
+
+-- Question 13
+-- Suppose that a website contains two tables, 
+-- the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+-- Table: Customers.
+
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Table: Orders.
+
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Using the above tables as example, return the following:
+
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+SELECT Name as customers
+FROM customers 
+WHERE Id NOT IN (SELECT CustomerId FROM orders) 
+
+-- ==================================================================================
+
